3771cf27127672d604645aee793e3b220a6eabe5
sdelaluzh <109712413+sdelaluzh@users.noreply.github.com>
2023-07-20 17:58:06 -0600

    Add files via upload

---
+++ ValidaDominios.py
@@ -0,0 +1,24 @@
+import socket
+
+def check_domain_status(domain):
+    try:
+        ip_address = socket.gethostbyname(domain)
+        return True, f"{domain} está en línea. IP: {ip_address}"
+    except socket.gaierror:
+        return False, f"No se puede resolver la IP de {domain}"
+
+def main():
+    domains = [
+        "www.example.com",
+        "www.google.com",
+        "www.openai.com",
+        "invalid-domain.com"  # Un dominio inválido para probar el manejo de errores
+    ]
+
+    for domain in domains:
+        status, message = check_domain_status(domain)
+        print(message)
+
+if __name__ == "__main__":
+    main()
+
